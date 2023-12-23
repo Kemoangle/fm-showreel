b6de9b513491068245c0b094b320652723a0c77f
John <56056004+vankiendhv@users.noreply.github.com>
2023-12-23 10:12:56 +0700

    update

--- FrontEnd/src/pages/second-page.vue
+++ FrontEnd/src/pages/second-page.vue
@@ -1,13 +1,84 @@
+<script lang="ts" setup>
+const listCategory = ref([
+    {
+        id: 1,
+        name: 'koko',
+        active: false,
+        children: [
+            {
+                id: 1,
+                name: 'children',
+                active: false,
+            },
+        ],
+    },
+    {
+        id: 2,
+        name: 'koko',
+        active: false,
+        children: [
+            {
+                id: 1,
+                name: 'children2',
+                active: false,
+            },
+        ],
+    },
+]);
+
+const menu = ref(false);
+</script>
+
 <template>
-  <div>
-    <VCard title="Create Awesome 🙌">
-      <VCardText>This is your second page.</VCardText>
-      <VCardText>
-        Chocolate sesame snaps pie carrot cake pastry pie lollipop muffin.
-        Carrot cake dragée chupa chups jujubes. Macaroon liquorice cookie
-        wafer tart marzipan bonbon. Gingerbread jelly-o dragée
-        chocolate.
-      </VCardText>
-    </VCard>
-  </div>
+    <div>
+        <VCard title="Create Awesome 🙌">
+            <VCardText>This is your second page.</VCardText>
+            <v-menu v-model="menu" :close-on-content-click="false" location="bottom">
+                <template v-slot:activator="{ props }">
+                    <v-btn color="indigo" v-bind="props"> Menu as Popover </v-btn>
+                </template>
+                <VCard>
+                    <VList>
+                        <div v-for="category in listCategory">
+                            <div class="father">
+                                <VCheckbox v-model="category.active" />
+                                <label>{{ category.name }}</label>
+                            </div>
+                            <div class="children" v-if="category.active">
+                                <div class="children-item" v-for="children in category.children">
+                                    <VCheckbox />
+                                    <label>{{ children.name }}</label>
+                                </div>
+                            </div>
+                        </div>
+                    </VList>
+                </VCard>
+            </v-menu>
+
+            <VCardText>
+                Chocolate sesame snaps pie carrot cake pastry pie lollipop muffin. Carrot cake
+                dragée chupa chups jujubes. Macaroon liquorice cookie wafer tart marzipan bonbon.
+                Gingerbread jelly-o dragée chocolate.
+            </VCardText>
+        </VCard>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+.father {
+    display: flex;
+    align-items: center;
+    padding-left: 10px;
+}
+p {
+    padding: 0;
+    margin: 0;
+}
+.children {
+    &-item {
+        padding-left: 40px;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
